e7f6ff83b740844959998e8c52df72fd5cf425d0
rulanugrh <nextarituenak01@gmail.com>
2023-07-04 09:36:35 +0700

    feat: profile apge

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,15 @@ module.exports = {
       colors: {
         primary: '#141529',
         secondary: '#F45050',
-        fortext: '#F9D949'
+        fortext: '#F9D949',
+        shadowBro: '#B2A4FF',
+        boxColor: '#330100',
+        docker: '#2496ED',
+        go: '#00ADD8',
+        bash: '#4EAA25',
+        k8s: '#326CE5',
+        telwin: '#06B6D4',
+        linux: '#FCC624'
       },
       fontFamily: {
         firaCode: "'Fira Code', monospace",
